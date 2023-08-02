fa3a02e96edf0ae383d34a0bd0ac840d87770f0d
Dan David <d_david8@yahoo.com>
2023-08-02 22:29:43 +0300

    exercise 1

---
+++ src/session1/homework/Exercise1.java
@@ -0,0 +1,17 @@
+package session1.homework;
+/*
+ *  1. Simple Output Program: Write a program that prints your name, age,
+ *  and favorite color on three separate lines.
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        //variable declaration
+        String myName = "David";
+        short myAge = 32;
+        String myFavoriteColor = "Turquoise";
+        //printing the result
+        System.out.println("My name is " + myName +
+                " I'm " + myAge +
+                " years old and my favorite color is " + myFavoriteColor);
+    }
+}
