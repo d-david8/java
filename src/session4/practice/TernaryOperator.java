2f093e2964450f32457f7442000c1c904be83219
Dan David <d_david8@yahoo.com>
2023-08-07 21:04:25 +0300

    session 4 practice

---
+++ src/session4/practice/TernaryOperator.java
@@ -0,0 +1,13 @@
+package session4.practice;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int firstScore = 100;
+        int secondScore = 200;
+
+        String result = firstScore > secondScore ? "First player won" : "Second player won";
+
+        System.out.println(result);
+    }
+}
