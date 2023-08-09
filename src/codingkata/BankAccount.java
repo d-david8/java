998d456cd81f37fdb18937579571f7c188f6d2cf
Dan David <d_david8@yahoo.com>
2023-08-09 20:02:55 +0300

    coding kata 1

---
+++ src/codingkata/BankAccount.java
@@ -0,0 +1,45 @@
+package codingkata;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String accountHolderName;
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(100.10);
+        bankAccount.withdraw(50.27);
+        bankAccount.getBalance();
+    }
+
+    //depositing method
+    public void deposit (double amount) {
+        if(isAmountValid(amount)) {
+            balance += amount;
+            System.out.println("Deposit: " + amount);
+        }else {
+            System.out.println("Invalid amount");
+        }
+    }
+    public void withdraw(double amount) {
+        if(isAmountValid(amount)) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("Deposit: " + amount);
+            }else {
+                System.out.println("Insufficient funds.");
+            }
+        }
+        else {
+            System.out.println("Invalid amount.");
+        }
+    }
+    public double getBalance(){
+        System.out.println("Your balance is: " + balance);
+        return balance;
+    }
+    private boolean isAmountValid(double value){
+        return value >=0;
+    }
+
+}
---
+++ src/codingkata/StringManipulation.java
@@ -0,0 +1,22 @@
+package codingkata;
+
+public class StringManipulation {
+
+    public static void main(String[] args) {
+
+        System.out.println(getStringLength("test"));
+        System.out.println(getStringReversed("abc"));
+    }
+
+    public static int getStringLength(String input) {
+        int count = 0;
+
+        for (char character : input.toCharArray()){
+            count++;
+        }
+        return count;
+    }
+    public static String getStringReversed(String input){
+        return new StringBuilder(input).reverse().toString();
+    }
+}
