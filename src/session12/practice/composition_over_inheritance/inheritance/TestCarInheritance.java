ef3bc90e08ee4e62837c2c156ee9b17d6c74a46b
Dan David <d_david8@yahoo.com>
2023-09-04 21:10:50 +0300

    Revert "homework 11"

--- src/session12/practice/composition_over_inheritance/inheritance/TestCarInheritance.java
+++
@@ -1,9 +0,0 @@
-package session12.practice.composition_over_inheritance.inheritance;
-
-public class TestCarInheritance {
-
-    public static void main(String[] args) {
-        Car car = new Car();
-        car.start();
-    }
-}
--- src/session12/practice/object_vs_reference/Car.java
+++
@@ -1,9 +0,0 @@
-package session12.practice.object_vs_reference;
-
-public class Car extends Vehicle {
-
-    @Override
-    public String fuelType() {
-        return "petrol";
-    }
-}
