2ef46ce7a582b75092f96dd39ff94770b3c8dacb
Dan David <d_david8@yahoo.com>
2023-09-06 22:36:53 +0300

    homework 12

---
+++ src/session12/homework/construction_management/ProjectStatus.java
@@ -0,0 +1,5 @@
+package session12.homework.construction_management;
+
+public enum ProjectStatus {
+    NOT_STARTED, IN_PROGRESS, BLOCKED, DONE;
+}
---
+++ src/session12/homework/construction_management/User.java
@@ -0,0 +1,38 @@
+package session12.homework.construction_management;
+
+public class User {
+
+    protected String userName;
+    protected String email;
+    protected Role role;
+    protected Project project;
+
+    User(String userName, Role role, Project project) {
+        this.userName = userName;
+        this.role = role;
+        this.email = userName + "@company.com";
+        this.project = project;
+    }
+
+    public void printProjectStatus() {
+        System.out.println("Status of the project: " + this.project.getProjectStatus());
+    }
+
+    public void printProjectResources() {
+        this.project.printResources();
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getDetails() {
+        return "User name: " + this.userName +
+                " email: " + this.email +
+                " role: " + this.role;
+    }
+}
